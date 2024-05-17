f17eb1622cc7ec48e323b23c061e584e5961f4ea
imber <a1157911285@163.com>
2024-05-17 23:34:06 +0800

    fix: 🐛 改为gif

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,82 @@
 'use client'
 import Link from 'next/link'
-import { useEffect } from 'react'
-
+import Image from 'next/image'
 export default function Page(): JSX.Element {
-  useEffect(() => {
-    const play = () => {
-      const videos = document.querySelectorAll('video')
-      videos.forEach((element) => {
-        element.play()
-      })
-    }
-    play()
-    document.addEventListener(
-      'WeixinJSBridgeReady',
-      function () {
-        play()
-      },
-      false
-    )
-  }, [])
   return (
     <div className="flex justify-center flex-col lg:pt-40 pt-8 pb-10">
       <div className="text-4xl text-center mb-10">Demos</div>
       <div className="mx-auto grid grid-cols-1 font-bold px-7 lg:px-20 gap-4 lg:grid-cols-3">
         <div className="border border-gray-400 rounded-lg overflow-hidden">
           <Link href="/normal">
-            <video src="/normal.mov" muted loop className="video"></video>
+            <div className="w-full">
+              <Image
+                src="/normal.gif"
+                width={600}
+                height={400}
+                alt="normal"
+                className="w-full h-full object-cover"
+              />
+            </div>
             <p className="p-4 text-lg">基础效果</p>
           </Link>
         </div>
 
         <div className="border border-gray-400 rounded-lg overflow-hidden">
           <Link href="/scene">
-            <video src="/skybox.mov" muted loop></video>
+            <div className="w-full">
+              <Image
+                src="/skybox.gif"
+                width={600}
+                height={400}
+                alt="normal"
+                className="w-full h-full object-cover"
+              />
+            </div>
             <p className="p-4 text-lg">场景贴图</p>
           </Link>
         </div>
 
         <div className="border border-gray-400 rounded-lg overflow-hidden">
           <Link href="/skybox">
-            <video src="/skybox.mov" muted loop></video>
+            <div className="w-full">
+              <Image
+                src="/skybox.gif"
+                width={600}
+                height={400}
+                alt="normal"
+                className="w-full h-full object-cover"
+              />
+            </div>
             <p className="p-4 text-lg">天空盒效果</p>
           </Link>
         </div>
 
         <div className="border border-gray-400 rounded-lg overflow-hidden">
           <Link href="/circleTexture">
-            <video src="/circleTexture.mov" muted loop></video>
+            <div className="w-full">
+              <Image
+                src="/circleTexture.gif"
+                width={600}
+                height={400}
+                alt="normal"
+                className="w-full h-full object-cover"
+              />
+            </div>
             <p className="p-4 text-lg">全景贴图效果</p>
           </Link>
         </div>
 
         <div className="border border-gray-400 rounded-lg overflow-hidden">
           <Link href="/earth" prefetch={false}>
-            <video src="/earth.mov" muted loop></video>
+            <div className="w-full">
+              <Image
+                src="/earth.gif"
+                width={600}
+                height={400}
+                alt="normal"
+                className="w-full h-full object-cover"
+              />
+            </div>
             <p className="p-4 text-lg">3D地球</p>
           </Link>
         </div>
