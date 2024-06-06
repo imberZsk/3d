50c8be70a11da09285cf49ae926f2ce2435c2819
imber <a1157911285@163.com>
2024-06-07 00:31:23 +0800

    feat: 3d看房增加指示物

--- app/circle-texture/page.tsx
+++ app/circle-texture/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import * as THREE from 'three'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 export default function Home() {
+  const tipRef = useRef<HTMLDivElement | null>(null)
   useEffect(() => {
     const scene = new THREE.Scene()
 
@@ -34,6 +35,108 @@ export default function Home() {
 
     controls.update()
 
+    const dataList = [
+      {
+        image: '/livingRoom.jpg',
+        tipsList: [
+          {
+            position: { x: -200, y: -4, z: -147 },
+            content: {
+              title: '进入厨房',
+              text: '',
+              image: 1,
+              showTip: false,
+              showTitle: true
+            }
+          }
+          // {
+          //   position: { x: -100, y: 0, z: -231 },
+          //   content: {
+          //     title: '信息点2',
+          //     text: '77989',
+          //     showTip: true,
+          //     showTitle: false
+          //   }
+          // },
+          // {
+          //   position: { x: 150, y: -50, z: -198 },
+          //   content: {
+          //     title: '信息点3',
+          //     text: 'qwdcz',
+          //     showTip: true,
+          //     showTitle: false
+          //   }
+          // },
+          // {
+          //   position: { x: 210, y: 111, z: -140 },
+          //   content: {
+          //     title: '信息点4',
+          //     text: '大豆食心虫侦察十大大苏打大大大大大大大',
+          //     showTip: true,
+          //     showTitle: false
+          //   }
+          // },
+          // {
+          //   position: { x: 208, y: -12, z: 140 },
+          //   content: {
+          //     title: '信息点5',
+          //     text: 'eq',
+          //     showTip: true,
+          //     showTitle: false
+          //   }
+          // },
+          // {
+          //   position: { x: 86, y: -9, z: 236 },
+          //   content: {
+          //     title: '进入房间',
+          //     text: '',
+          //     showTip: false,
+          //     showTitle: true
+          //   }
+          // }
+        ]
+      }
+    ]
+
+    const addTipsSprite = (index = 0) => {
+      let tipTexture = new THREE.TextureLoader().load('/tip.png')
+      let material = new THREE.SpriteMaterial({ map: tipTexture })
+      // const tipsSpriteList = []
+      dataList[index].tipsList.forEach((item) => {
+        let sprite = new THREE.Sprite(material)
+        sprite.scale.set(10, 10, 10)
+        sprite.position.set(item.position.x, item.position.y, item.position.z)
+        // sprite.content = item.content
+        // tipsSpriteList.push(sprite)
+        scene.add(sprite)
+      })
+    }
+
+    addTipsSprite()
+
+    // const onMouseClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    //   e.preventDefault()
+    //   let element = this.$refs.threeDBox
+    //   let raycaster = new THREE.Raycaster()
+    //   let mouse = new THREE.Vector2()
+    //   mouse.x = (e.clientX / element.clientWidth) * 2 - 1
+    //   mouse.y = -(e.clientY / element.clientHeight) * 2 + 1
+
+    //   raycaster.setFromCamera(mouse, camera)
+    //   let intersects = raycaster.intersectObjects(this.tipsSpriteList, true)
+    //   if (intersects.length > 0 && intersects[0].object.content.showTitle) {
+    //     this.changeContentAndtips(intersects[0].object.content.image)
+    //     this.handleTooltipHide(e)
+    //   }
+    //   // const x = e.clientX;
+    //   // const y = e.clientY;
+
+    //   // const pos = this.screenToWorld(x, y);
+    //   // console.log(pos);
+    // }
+
+    // window.addEventListener('click', onMouseClick, false)
+
     function animate() {
       requestAnimationFrame(animate)
       controls.update()
@@ -46,5 +149,15 @@ export default function Home() {
     }
   }, [])
 
-  return <main id="container"></main>
+  return (
+    <main>
+      <div id="container"></div>
+      <div className="tooltip-box" ref={tipRef}>
+        <div className="container">
+          <div className="title">标题：1</div>
+          <div className="explain">说明：2</div>
+        </div>
+      </div>
+    </main>
+  )
 }
